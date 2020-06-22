7e4a1da6141085cdf6d3064ff9b9d9bb341e9cbd
Alan Ridlehoover <alan@ridlehoover.com>
2020-06-22 00:27:54 -0700

    Adds ability to toggle between average and total flog score by clicking on the status bar item
    Adds ability to see total flog score for selected text
    
    Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

--- extension.js
+++ extension.js
@@ -1,50 +1,153 @@
 const vscode = require('vscode');
 const cp = require('child_process');
 
-let myStatusBarItem;
+let statusBarItem;
+let handle;
+let mode = 'average';
+let state = 'deselected';
+let total;
+let average;
 
 function activate({subscriptions}) {
-
   const commandId = 'ruby-flog.average';
-  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
-  myStatusBarItem.command = commandId;
-  subscriptions.push(myStatusBarItem);
 
-  subscriptions.push(vscode.commands.registerCommand(commandId, updateStatusBarItem));
-  subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-  subscriptions.push(vscode.workspace.onDidChangeTextDocument(updateStatusBarItem));
+  statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+  statusBarItem.command = commandId;
+  subscriptions.push(statusBarItem);
 
-  updateStatusBarItem();
+  subscriptions.push(vscode.commands.registerCommand(commandId, toggle));
+  subscriptions.push(vscode.window.onDidChangeActiveTextEditor(initialize));
+  subscriptions.push(vscode.window.onDidChangeTextEditorSelection(changeSelection));
+  subscriptions.push(vscode.workspace.onDidChangeTextDocument(edit));
+
+  initialize();
 }
 exports.activate = activate;
 
-function updateStatusBarItem() {
-  const activeTextEditor = vscode.window.activeTextEditor;
+// Private functions
 
-  if (activeTextEditor === undefined) { return; }
+function debounce(updateMethod) {
+  clearTimeout(handle);
+  handle = setTimeout(updateMethod, 200);
+}
 
-  const document = activeTextEditor.document;
-  const languageId = document.languageId;
+function initialize() {
+  transitionState('initialize');
+  debounce(update);
+}
 
-  if (languageId !== 'ruby') { return myStatusBarItem.hide(); }
+function toggle() {
+  transitionMode();
+  render();
+}
 
-  cp.exec(flogCommand(escapedText(document)), (_, flogResult) => {
-    updateFlogScore(flogResult);
+function edit() {
+  debounce(update);
+}
+
+function changeSelection() {
+  if (!getSelection().isEmpty) {
+    transitionState('select');
+    debounce(update);
+  } else if (state === 'selected') {
+    transitionState('deselect');
+    debounce(update);
+  }
+}
+
+function transitionMode() {
+  mode = mode === 'average' ? 'total' : 'average';
+}
+
+function transitionState(event) {
+  state = event === 'select' ? 'selected' : 'deselected';
+}
+
+function update() {
+  const text = state === 'selected' ? getText(getSelection()) : getText();
+  if (text === undefined) { return; }
+
+  showLoadingState();
+
+  cp.exec(flogCommand(text), (err, stdout) => {
+    if (err) { return; }
+
+    const scores = getFlogScores(stdout);
+    total = scores.total;
+    average = scores.average;
+
+    render();
   });
 }
 
-function escapedText(document) {
-  return document.getText().replace(/"/g, '\\"');
+function render() {
+  if (state === 'selected') {
+    showFlogScore(`Total Selected Flog: ${total}`);
+  } else if (mode === 'total') {
+    showFlogScore(`Total Flog: ${total}`);
+  } else {
+    showFlogScore(`Average Flog: ${average}`);
+  }
 }
 
-function flogCommand(documentText) {
-  const changeToStdin = `echo "${documentText}"`;
+function getText(range) {
+  const document = getActiveDocument();
+
+  if (!isValidDocument(document)) {
+    statusBarItem.hide();
+    return;
+  }
+
+  if (range === undefined || range.isEmpty) {
+    return document.getText();
+  } else {
+    return document.getText(range);
+  }
+}
+
+function getActiveDocument() {
+  const activeTextEditor = getActiveTextEditor();
+
+  if (activeTextEditor === undefined) { return; }
+
+  return activeTextEditor.document;
+}
+
+function getSelection() {
+  return getActiveTextEditor().selection;
+}
+
+function getActiveTextEditor() {
+  return vscode.window.activeTextEditor;
+}
+
+function showLoadingState() {
+  statusBarItem.text = "Calculating Flog $(tree-item-loading)";
+  statusBarItem.show();
+}
+
+function showFlogScore(message) {
+  statusBarItem.text = message;
+  statusBarItem.show();
+}
+
+function isValidDocument(document) {
+  return (
+    document !== undefined &&
+    document.languageId === 'ruby'
+  );
+}
+
+function getFlogScores(flogResult) {
+  const lines = flogResult.split("\n");
+  const total = lines[0].split(":")[0].trim();
+  const average = lines[1].split(":")[0].trim();
+  return { total, average };
+}
+
+function flogCommand(text) {
+  const escapedText = text.replace(/"/g, '\\"');
+  const changeToStdin = `echo "${escapedText}"`;
   const flogFromStdin = `ruby -e "require 'flog_cli'; FlogCLI.new(FlogCLI.parse_options(ARGV)).tap { |f| f.flog('-'); f.report }"`;
   return `${changeToStdin} | ${flogFromStdin}`;
-}
-
-function updateFlogScore(flogResult) {
-  const averageScore = flogResult.split("\n")[1].split(":")[0].trim();
-  myStatusBarItem.text = `Average Flog: ${averageScore}`;
-  myStatusBarItem.show();
 }
